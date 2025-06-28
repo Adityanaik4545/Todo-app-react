cbea4724529e73276b4fa87374d6c86ad242c915
Adityanaik4545 <adityank23@outlook.com>
2025-06-29 03:43:58 +0530

    done with two functionallity adding todo and checkmark the todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleEdit=()=>{
+    
+  }
+  const handleDelete=()=>{
+
+  }
+  const handleAdd=()=>{
+      setTodos([...todos,{id:uuidv4(), todo, isCompleted: false}])
+      setTodo("")
+  }
+  const handleChange=(e)=>{
+      setTodo(e.target.value)
+      
+    }
+  const handleCheckbox=(e)=>{
+      let id=e.target.name
+      let index=todos.findIndex(item=>{
+        return item.id===id;
+      })
+      let newToddos=[...todos];
+      newToddos[index].isCompleted=!newToddos[index].isCompleted;
+      setTodos(newToddos)
+    }
 
   return (
     <>
@@ -12,18 +38,21 @@ function App() {
   <div className="container mx-auto my-4 bg-violet-100 p-5 rounded-2xl min-h-[80vh]">
       <div className="addtodo my-5">
       <h1 className='text-3xl font-semibold'>Add Todos</h1>
-      <input type="text" className='bg-white w-1/2' />
-      <button className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-2 rounded-lg transition-all duration-400 font-semibold'>Add</button>
+      <input onChange={handleChange} value={todo} type="text" className='bg-white w-1/2' />
+      <button onClick={handleAdd} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-2 rounded-lg transition-all duration-400 font-semibold'>Add</button>
       </div>
       <h2 className='text-lg font-bold'>Your Todos</h2>
       <div className="todos">
-        <div className="todo flex">
-        <div className="text">Lorem ipsum dolor, sit amet consectetur  sapiente tempora alias suscipit </div>
+        {todos.map(item=>{
+        return <div className="todo flex w-1/2 justify-between my-3">
+          <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} />
+        <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
         <div className="btns">
-          <button className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Edit</button>
-          <button className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Delete</button>
+          <button onClick={handleEdit} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Edit</button>
+          <button onClick={handleDelete} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Delete</button>
         </div>
         </div>
+        })}
       </div>
   </div>
     </>
