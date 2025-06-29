ef9c468519ad0c6dbb1d250d1a68cf2c511f898f
Adityanaik4545 <adityank23@outlook.com>
2025-06-29 18:36:38 +0530

    delete function done

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,58 @@ function App() {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
 
-  const handleEdit=()=>{
-    
-  }
-  const handleDelete=()=>{
+  const handleEdit = () => {
 
   }
-  const handleAdd=()=>{
-      setTodos([...todos,{id:uuidv4(), todo, isCompleted: false}])
-      setTodo("")
+  const handleDelete = (e,id) => {
+    let newTodos=todos.filter(item=>{
+      return item.id!==id
+    })
+    setTodos(newTodos)
+    
   }
-  const handleChange=(e)=>{
-      setTodo(e.target.value)
-      
-    }
-  const handleCheckbox=(e)=>{
-      let id=e.target.name
-      let index=todos.findIndex(item=>{
-        return item.id===id;
-      })
-      let newToddos=[...todos];
-      newToddos[index].isCompleted=!newToddos[index].isCompleted;
-      setTodos(newToddos)
-    }
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
+    setTodo("")
+  }
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+
+  }
+  const handleCheckbox = (e) => {
+    let id = e.target.name
+    let index = todos.findIndex(item => {
+      return item.id === id;
+    })
+    let newToddos = [...todos];
+    newToddos[index].isCompleted = !newToddos[index].isCompleted;
+    setTodos(newToddos)
+  }
 
   return (
     <>
-  <Navbar/>
-  <div className="container mx-auto my-4 bg-violet-100 p-5 rounded-2xl min-h-[80vh]">
-      <div className="addtodo my-5">
-      <h1 className='text-3xl font-semibold'>Add Todos</h1>
-      <input onChange={handleChange} value={todo} type="text" className='bg-white w-1/2' />
-      <button onClick={handleAdd} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-2 rounded-lg transition-all duration-400 font-semibold'>Add</button>
-      </div>
-      <h2 className='text-lg font-bold'>Your Todos</h2>
-      <div className="todos">
-        {todos.map(item=>{
-        return <div className="todo flex w-1/2 justify-between my-3">
-          <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} />
-        <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
-        <div className="btns">
-          <button onClick={handleEdit} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Edit</button>
-          <button onClick={handleDelete} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Delete</button>
+      <Navbar />
+      <div className="container mx-auto my-4 bg-violet-100 p-5 rounded-2xl min-h-[80vh]">
+        <div className="addtodo my-5">
+          <h1 className='text-3xl font-semibold'>Add Todos</h1>
+          <input onChange={handleChange} value={todo} type="text" className='bg-white w-1/2' />
+          <button onClick={handleAdd} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-2 rounded-lg transition-all duration-400 font-semibold'>Add</button>
         </div>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <div className="todos">
+          {todos.length==0 && <div className='m-3'>No Todos</div> }
+          {todos.map(item => {
+            return <div className="todo flex w-1/2 justify-between my-3">
+              <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} />
+              <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+              <div className="btns">
+                <button onClick={handleEdit} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Edit</button>
+                <button onClick={(e)=>{handleDelete(e,item.id)}} className='bg-violet-400 hover:bg-violet-600 hover:text-white px-5 mx-1 rounded-lg transition-all duration-400 font-semibold'>Delete</button>
+              </div>
+            </div>
+          })}
         </div>
-        })}
       </div>
-  </div>
     </>
   )
 }
