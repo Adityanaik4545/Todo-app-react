d8c4429a6c307ab19d0f8d157827b0b916cce977
Adityanaik4545 <adityank23@outlook.com>
2025-06-30 19:56:11 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between bg-violet-500 text-white py-2 '>
         <div className="logo font-bold text-lg mx-3">
-            Task
+            U-Task
         </div>
         <ul className='flex font-semibold gap-8 mx-9'>
             <li className='cursor-pointer hover:font-light'>Home</li>
